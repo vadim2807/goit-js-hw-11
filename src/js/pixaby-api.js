e1204af680ffb2fc31e2a1fbbc66a1639ad4e0b7
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-07-20 19:09:18 +0300

    вторая попытка первой сдачи двойной тостер

---
+++ src/js/pixaby-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export function getImagesByQuery(query) {
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '51391148-cca0cd30758df7ebb9b563956', // Замени на свой ключ
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 9,
+    },
+  });
+}
---
+++ src/main.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './js/pixaby-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
+
+// Инициализация внутренних данных
+export const form = document.querySelector('.form');
+export const input = document.querySelector('input[name="search-text"]');
+export const loader = document.querySelector('#loader');
+export const gallery = document.querySelector('.gallery');
+
+// Обработка формы
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const query = input.value.trim();
+
+  if (!query) {
+    showIziError('Поле пошуку не може бути порожнім');
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(response => {
+        hideLoader();
+        input.value = "";
+      const data = response.data;
+      if (data.hits.length === 0) {
+        showIziError('Sorry, there are no images matching your search query. Please try again!');
+      } else {
+        createGallery(data.hits);
+      }
+    })
+    .catch(error => {
+      hideLoader();
+      showIziError('Щось пішло не так... Ми вже працюємо над цією проблемою.');
+      console.log(error);
+    });
+});
+
+// Функция для показа ошибок
+function showIziError(txt) {
+  iziToast.error({
+    title: 'Error',
+    message: `${txt}`,
+    progressBar: false,
+    transitionIn: 'fadeIn',
+    position: 'topRight',
+    animateInside: true,
+  });
+}
